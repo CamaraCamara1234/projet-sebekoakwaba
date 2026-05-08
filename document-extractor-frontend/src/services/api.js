e71f392a94908203eb3c8e9b763f1cfc60ff65dd
CamaraCamara1234 <aboudramanecamara236@gmail.com>
2026-05-08 22:18:27 +0000

    change api_base url

--- document-extractor-frontend/src/services/api.js
+++ document-extractor-frontend/src/services/api.js
@@ -1,6 +1,6 @@
 // const API_BASE = 'https://checkid.akwabasebeko.com';
-// const API_BASE = 'https://jonna-unstrung-sickeningly.ngrok-free.dev';
-const API_BASE = 'http://127.0.0.1:8000';
+const API_BASE = 'https://jonna-unstrung-sickeningly.ngrok-free.dev';
+// const API_BASE = 'http://127.0.0.1:8000';
 
 /**
  * Résout une URL d'image relative /media/... vers l'URL complète du serveur.
